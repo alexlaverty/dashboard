573cbd4b7b350e1d7c21caf563ff687ef05a92f8
Alex Laverty <alaverty@gmail.com>
2024-02-17 19:49:13 +1100

    awdawdwad

--- update_fitbit_token.py
+++ update_fitbit_token.py
@@ -12,7 +12,7 @@ GH_TOKEN = os.environ.get('GH_TOKEN')
 REPO_OWNER = 'alexlaverty'
 REPO_NAME = 'dashboard'
 SECRET_NAME = 'FITBIT_TOKEN'
-FITBIT_TOKEN = os.environ.get('FITBIT_TOKEN')
+FITBIT_TOKEN = json.loads(os.environ.get('FITBIT_TOKEN'))
 
 secrets = [CLIENT_ID, CLIENT_SECRET, GH_TOKEN, FITBIT_TOKEN]
 
