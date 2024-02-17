aa107e007bacf612b74bc83e39b1b4d910a1f9bc
Alex Laverty <alaverty@gmail.com>
2024-02-17 19:34:20 +1100

    print secrets

--- update_fitbit_token.py
+++ update_fitbit_token.py
@@ -1,4 +1,4 @@
-import fitbit
+import fitbit as fb
 import json
 import os
 import requests
@@ -12,45 +12,12 @@ GH_TOKEN = os.environ.get('GH_TOKEN')
 REPO_OWNER = 'alexlaverty'
 REPO_NAME = 'dashboard'
 SECRET_NAME = 'FITBIT_TOKEN'
+FITBIT_TOKEN = os.environ.get('FITBIT_TOKEN')
 
-# Initialize the Fitbit API client
-client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET)
+secrets = [CLIENT_ID, CLIENT_SECRET, GH_TOKEN, FITBIT_TOKEN]
 
-try:
-    # Read existing token JSON from GitHub Actions repository secret
-    token_secret_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/secrets/{SECRET_NAME}'
-    headers = {
-        'Authorization': f'token {GH_TOKEN}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    response = requests.get(token_secret_url, headers=headers)
-    response.raise_for_status()
-    response_json = response.json()
-    print(response_json)
-    existing_token = json.loads(response_json['secret']['value'])
+def print_secret(secret):
+    print(' '.join(secret))
 
-    # Refresh the access token
-    new_token = client.client.refresh_token()
-
-    # Save the new token to token.json
-    with open('token.json', 'w') as f:
-        json.dump(new_token, f)
-
-    # Update the FITBIT_TOKEN secret in the GitHub Actions repository
-    update_secret_url = f'https://api.github.com/repos/{REPO_OWNER}/{REPO_NAME}/actions/secrets/{SECRET_NAME}'
-    update_payload = {
-        'encrypted_value': new_token['access_token'],
-        'key_id': response_json['key_id']
-    }
-    update_response = requests.put(update_secret_url, headers=headers, json=update_payload)
-    update_response.raise_for_status()
-
-    # Get the latest weight measurement
-    weight_data = client.get_bodyweight(user_id='-', base_date='today')
-    latest_weight = weight_data['weight'][-1]
-    print(f"Latest weight measurement: {latest_weight}")
-
-finally:
-    # Always delete the token.json file at the end of the script
-    if os.path.exists('token.json'):
-        os.remove('token.json')
+for secret in secrets:
+    print_secret(secret)
