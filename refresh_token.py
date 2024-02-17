5d6aaca463901e74dec1ba70b7980535c4527780
Alex Laverty <alaverty@gmail.com>
2024-02-17 20:19:06 +1100

    awdawdwad

--- refresh_token.py
+++ refresh_token.py
@@ -3,18 +3,22 @@ import os
 from base64 import b64encode
 from pathlib import Path
 from typing import Any, Dict
-
+import json
 import requests
 from nacl import encoding
 from nacl.public import PublicKey, SealedBox
 from requests import Response
+import fitbit as fb
+import csv
 
 github_user = "alexlaverty"
 github_repo = "dashboard"
 github_token = os.getenv("GH_TOKEN")
-secret_name = "MY_FITBIT_TOKEN"  # noqa: S105
+secret_name = "FITBIT_TOKEN"  # noqa: S105
 api_endpoint = f"https://api.github.com/repos/{github_user}/{github_repo}"
-
+CLIENT_ID = os.environ.get('FITBIT_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('FITBIT_SECRET')
+FITBIT_TOKEN = json.loads(os.environ.get('FITBIT_TOKEN'))
 
 def get_repo_key() -> Dict:
     """Get the key to a GitHub repository.
@@ -88,18 +92,47 @@ def get_file_contents(filename: Path) -> str:
     with open(filename, "r") as f:
         return f.read()
 
+def write_token_dict(token_dict):
+    print("refreshed_token dict :")
+    file_contents: str = str(token_dict)
 
-file_contents: str = "THIS IS MY TOKEN BRO"
-print("file_contents:")
-print(file_contents)
+    print("file_contents:")
+    print(file_contents)
+    repo_key: Any = get_repo_key()
 
-repo_key: Any = get_repo_key()
+    print("repo_key:")
+    print(repo_key)
 
-print("repo_key:")
-print(repo_key)
+    encrypted_secret: str = encrypt(repo_key["key"], file_contents)
+    print("encrypted_secret:")
+    print(encrypted_secret)
 
-encrypted_secret: str = encrypt(repo_key["key"], file_contents)
-print("encrypted_secret:")
-print(encrypted_secret)
+    write_secret(repo_key, encrypted_secret)
 
-write_secret(repo_key, encrypted_secret)
+authd_client = fb.Fitbit(
+                    CLIENT_ID,
+                    CLIENT_SECRET,
+                    access_token=FITBIT_TOKEN["access_token"],
+                    refresh_token=FITBIT_TOKEN["refresh_token"],
+                    expires_in=FITBIT_TOKEN["expires_in"],
+                    refresh_cb=write_token_dict
+                )
+
+# Get weight values
+weight_data = authd_client.get_bodyweight(period='7d')
+
+# Define CSV file path
+csv_file_path = "data.csv"
+
+if 'body-weight' in weight_data['weight']:
+    weight_values = weight_data['weight']['body-weight']
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date", "Weight"])
+        for weight_entry in weight_values:
+            date = weight_entry['dateTime']
+            weight = weight_entry['value']
+            writer.writerow([date, weight])
+    print(f"Weight data written to '{csv_file_path}' successfully.")
+else:
+    print("No weight data found.")
