7e2e257059532416dd5d8a29ff02e7e3199c67a7
Alex Laverty <alaverty@gmail.com>
2024-02-18 13:48:35 +1100

    updates fitbit period

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ github_user = "alexlaverty"
 github_repo = "dashboard"
 github_token = os.getenv("GH_TOKEN")
 api_endpoint = f"https://api.github.com/repos/{github_user}/{github_repo}"
-fitbit_period = '90d' # Supported: 1d | 7d | 30d | 1w | 1m
+fitbit_period = '3m' # Supported: 1d | 7d | 30d | 1w | 1m
 csv_file = "data.csv"
 
 FITBIT_CLIENT_ID = os.environ.get('FITBIT_CLIENT_ID')
